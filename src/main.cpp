52c191c5ce9700430fc5d3e83ce1407587226dfd
Sourav Chatterjee <sourav.chatterjee@unity3d.com>
2024-07-20 20:13:52 +0300

    First trivial test added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+void testCanBeCreated()
+{
+    std::cout << "Test: can be created" << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    testCanBeCreated();
 
     return 0;
 }
