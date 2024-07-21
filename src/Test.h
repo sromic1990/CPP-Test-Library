e77767634576b5798d718790757058cb20f56c2b
Sourav Chatterjee <sourav.chatterjee@unity3d.com>
2024-07-22 02:26:32 +0300

    Adding condition to check for missed failures, such that tests which were supossed to fail, must not accidentally pass.

--- src/Test.h
+++ src/Test.h
@@ -6,25 +6,39 @@
 
 namespace SouravTDD
 {
+    class MissingException
+    {
+        public:
+            MissingException(std::string_view exType) : mExType(exType) {}
+            std::string_view getExType() const { return mExType; }
+
+        private:
+            std::string_view mExType;
+    };
+
     class TestBase
     {
         public:
             TestBase(std::string_view name) : mName(name), mPassed(true) {}
             virtual ~TestBase() = default;
             virtual void run() = 0;
+            virtual void runEx() { run (); }
             std::string_view getName() const { return mName; }
             bool passed() const { return mPassed; }
             std::string_view getReason() const { return mReason; }
+            std::string_view getExpectedReason() const { return mExpectedReason; }
             void setFailed(std::string_view reason) 
             { 
                 mPassed = false; 
                 mReason = reason; 
             }
+            void setExpectedFailureReason(std::string_view reason) { mExpectedReason = reason; }
 
         private:
             std::string_view mName;
             bool mPassed;
             std::string_view mReason;
+            std::string_view mExpectedReason;
     };
 
     inline std::vector<TestBase*>& getTests()
@@ -41,6 +55,7 @@ namespace SouravTDD
 
         int numPassed = 0;
         int numFailed = 0;
+        int numMissedFailed = 0;
 
         for (auto * test : getTests())
         {
@@ -50,18 +65,43 @@ namespace SouravTDD
             
             try
             {
-                test->run();   
+                test->runEx();   
+            }
+            catch(MissingException const & ex)
+            {
+                std::string message = "Expected exception type ";
+                message += ex.getExType();
+                message += " was not thrown.";
+                test->setFailed(message);
             }
             catch(...)
             {
-                test->setFailed("Unexpected exception thrown");
+                test->setFailed("Unexpected exception thrown.");
             }
 
             if (test->passed())
             {
-                ++numPassed;
-                output << "PASSED\n";
+                if(!test->getExpectedReason().empty())
+                {
+                    ++numMissedFailed;
+                    output << "Missed expected failure\n"
+                        << "Test passed but was expected to fail."
+                        << std::endl;
+                }
+                else
+                {
+                    ++numPassed;
+                    output << "PASSED\n";
+                }
             }
+            else if (!test->getExpectedReason().empty() && test->getReason() == test->getExpectedReason())
+            {
+                ++numPassed;
+                output  << "Expected failure\n"
+                        << test->getExpectedReason()
+                        << std::endl;
+            }
+            
             else
             {
                 ++numFailed;
@@ -70,14 +110,12 @@ namespace SouravTDD
         }
 
         output << "---------------\n";
-        if (numFailed == 0)
+        output << "Tests passed: " << numPassed << "\nTests failed: " << numFailed << std::endl;
+        if (numMissedFailed != 0)
         {
-            output << "All tests passed" << std::endl;
+            output << "Test failures missed: " << numMissedFailed << std::endl;
         }
-        else
-        {
-            output << "Tests passed: " << numPassed << "\nTests failed: " << numFailed << std::endl;
-        }
+        output << std::endl;
         return numFailed;
     }
 }
@@ -96,6 +134,31 @@ class SOURAVTDD_CLASS : public SouravTDD::TestBase \
             {\
                 SouravTDD::getTests().push_back(this);\
             }\
+        void run() override;\
+}; \
+SOURAVTDD_CLASS SOURAVTDD_INSTANCE (testname); \
+void SOURAVTDD_CLASS::run()
+
+#define TEST_EX(testname, exceptionType) \
+class SOURAVTDD_CLASS : public SouravTDD::TestBase \
+{\
+    public: \
+        SOURAVTDD_CLASS (std::string_view name) : TestBase(name) \
+            {\
+                SouravTDD::getTests().push_back(this);\
+            }\
+        void runEx() override\
+        {\
+            try\
+            {\
+                run();\
+            }\
+            catch(exceptionType const &) \
+            {\
+                return;\
+            }\
+            throw SouravTDD::MissingException(#exceptionType);\
+        }\
         void run() override;\
 }; \
 SOURAVTDD_CLASS SOURAVTDD_INSTANCE (testname); \
